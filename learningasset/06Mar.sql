fe8995db3b39675b9b71873b52e689230a44e05e
Sivaprakash KK <sivaprakash.kk@hpe.com>
2018-03-08 15:07:40 +0530

    Learning files, SQL and class path

---
+++ learningasset/06Mar.sql
@@ -0,0 +1,16 @@
+
+---------------------------------------
+show databases;
+use hibernatedb;
+show tables;
+select * from user_details;
+select * from user_collection;
+select * from user_address;
+select * from Vehicle;
+select * from USER_VEHICLE;
+
+drop table USER_VEHICLE ;
+drop table  user_entity;
+drop table  user_collection;
+drop table  user_address;
+drop table vehicle;
